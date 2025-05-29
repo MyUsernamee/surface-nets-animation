e07ab5346e4183b1bd88a7c3a087dce825509d4a
MyUsernamee <delanoleslieprof@gmail.com>
2025-05-29 16:44:48 -0700

    Add key pausing

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -2,38 +2,54 @@ class_name Animator
 extends Node2D
 
 var frames = [];
+var waiting = false;
 @onready var tween = get_tree().create_tween();
 @export var camera: Camera3D;
 
-func draw_method(method, duration, ease_time=0.5):
-
+func draw_method(method, duration, ease_time=0.5, add_pause=false):
 
 	tween.tween_method(method
 	, 0.0, 1.0, ease_time);
 
-	tween.tween_method(method
-	, 1.0, 1.0, duration);
+	if add_pause:
+		wait_for_key()
+	else:
+		tween.tween_method(method
+		, 1.0, 1.0, duration);
 
 	tween.tween_method(method
 	, 1.0, 0.0, ease_time);
 
 
-func draw_text(text, position, duration, ease_time=0.5, size=32, color=Color.WHITE):
+func draw_text(text, position, duration, ease_time=0.5, size=32, color=Color.WHITE, add_pause = false):
 
 	draw_method(func draw_hello(x):
 		DebugDraw3D.draw_text(position, text, size, Color(color.r, color.g, color.b, x))
-	, duration, ease_time);
+	, duration, ease_time, add_pause);
 
 func set_camera(position, size):
 
 	tween.tween_property(camera, "position",  position, 1.0,)
 	tween.parallel().tween_property(camera, "size", size, 1.0,)
 
+func wait_for_key():
+	tween.tween_callback(func temp():
+		waiting = true;
+	)
+
 func _ready():
 
 	tween.set_trans(Tween.TRANS_EXPO)
+	tween.pause()
 
-	draw_text("Hello!", Vector3.ZERO, 1.0, 0.5, 128)
-	tween.set_parallel(true)
-	set_camera(Vector3.ZERO, 10.0)
-	tween.set_parallel(false)
+	draw_text("Surface Nets", Vector3.ZERO, 1.0, 0.5, 128, Color.WHITE, true)
+	set_camera(Vector3(0.0, 0.0, 10.0), 10.0)
+	draw_text("I am testing it!", Vector3.ZERO, 8.0, 0.5, 128, Color.WHITE, true)
+
+func _process(delta: float) -> void:
+
+	if waiting:
+		waiting = waiting and not Input.is_key_pressed(KEY_SPACE)
+		tween.custom_step(0.0000000000001)
+	else:
+		tween.custom_step(delta)
