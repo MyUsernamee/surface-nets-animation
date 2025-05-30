e82820d3ae52bc756242e2244e99e70d1f165ef9
MyUsernamee <delanoleslieprof@gmail.com>
2025-05-30 10:27:00 -0700

    Add circle

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -37,14 +37,45 @@ func wait_for_key():
 		waiting = true;
 	)
 
+@onready var circle_resource: PackedScene = load("res://scenes/circle.tscn")
+@onready var circle_outline_resource: PackedScene = load("res://scenes/circle_outline.tscn")
+
+func add_circle():
+	var new_circle: Sprite3D = circle_resource.instantiate();
+	add_child(new_circle)
+	return new_circle
+
+func add_circle_outline():
+	var new_circle: Sprite3D = circle_outline_resource.instantiate();
+	add_child(new_circle)
+	return new_circle
+
+
 func _ready():
 
 	tween.set_trans(Tween.TRANS_EXPO)
 	tween.pause()
 
 	draw_text("Surface Nets", Vector3.ZERO, 1.0, 0.5, 128, Color.WHITE, true)
-	set_camera(Vector3(0.0, 0.0, 10.0), 10.0)
-	draw_text("I am testing it!", Vector3.ZERO, 8.0, 0.5, 128, Color.WHITE, true)
+	draw_text("(INSERT SOME INSPIRATIONAL PHOTOS HERE LMAO)", Vector3.ZERO, 8.0, 0.5, 128, Color.WHITE, true)
+	draw_text("SDF -> Mesh", Vector3.ZERO, 1.0, 0.25, 128, Color.WHITE, true)
+	draw_text("SDF?????", Vector3.ZERO, 1.0, 0.25, 128, Color.WHITE, true)
+	draw_text("d(x) -> float", Vector3.ZERO, 1.0, 0.25, 128, Color.WHITE, true) # TODO: Latex
+	var reference_point = add_circle()
+	reference_point.modulate = Color(1.0, 0.25, 0.25, 0);
+	reference_point.scale = Vector3.ONE * 0.1;
+	reference_point.position = Vector3(-1.0, 1.0, 0.0)
+	var sdf_outline = add_circle_outline()
+	sdf_outline.reparent(reference_point)
+	sdf_outline.scale = Vector3.ZERO;
+	sdf_outline.position = Vector3.ZERO;
+	tween.tween_property(reference_point, "modulate:a", 1.0, 1.0);
+	wait_for_key()
+	tween.tween_callback(func temp():
+		sdf_outline.modulate.a = 1.0
+	)
+	tween.tween_property(sdf_outline, "scale", Vector3.ONE * 10.0, 0.2);
+
 
 func _process(delta: float) -> void:
 
